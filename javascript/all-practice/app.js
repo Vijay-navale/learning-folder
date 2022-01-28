69811798b370f0abcb2020a5e942c04824721a94
WebDeveloper01 <webdevelopment@togglehead.in>
2022-01-29 01:06:14 +0530

    daily learning - javascript

--- javascript/all-practice/app.js
+++ javascript/all-practice/app.js
@@ -1,18 +1,6 @@
-/*=============
-    spread and rest parameter
-===============*/
-var myObj = {};
-console.log(myObj);
-Object.assign(myObj, { a: 1, b: 2 }, { z: 5 });
-console.log(myObj); //{a: 1, b: 2, z: 5}
-
-function sumOne(a, b) {
-  return a + b;
+var aa = 5;
+function aaName() {
+  console.log("Namaste vijay");
 }
-console.log(sumOne(...[5, 4])); //spread
-
-function sumTwo(...args) {
-  //rest - always gives array
-  console.log(args);
-}
-console.log(sumTwo({}, {}));
+console.log(aa);
+aaName();
