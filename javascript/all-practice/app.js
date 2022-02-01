8073ef1649cd14de9d610e3c6c0ff0a4db82e2d3
WebDeveloper01 <webdevelopment@togglehead.in>
2022-02-02 00:33:14 +0530

    daily learning - javascript

--- javascript/all-practice/app.js
+++ javascript/all-practice/app.js
@@ -63,11 +63,45 @@
 //   }
 // }
 // a();
-/*=============
-    let & const in JS 🔥Temporal Dead Zone 
-===============*/
+// /*=============
+//     let & const in JS 🔥Temporal Dead Zone
+// ===============*/
 
-let a;
-const c;
-var aa;
-console.log(c);
+// let a = 10;
+// // const c;
+// var aa = 2;
+// var aa = 1;
+// console.log(aa);
+
+/*=============
+     BLOCK SCOPE & Shadowing in JS 🔥 
+ ===============*/
+{
+  var a = 10;
+  const b = 100;
+  let c = 1000;
+}
+console.log(a);
+
+if (true) {
+  var z = 5;
+}
+console.log(z);
+
+// function d() {
+//   //this is altogether new stacking context
+//   var f = 4;
+// }
+// d();
+// console.log(f);
+
+let w = 10;
+{
+  let w = 100;
+}
+console.log(w);
+const e = 10;
+{
+  const e = 100;
+  console.log(e);
+}
